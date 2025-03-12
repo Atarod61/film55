900f91c5e8c071ed2e825c244ea2ee8b7f14b253
Atarod61 <atarod2017@gmail.com>
2025-03-12 11:19:47 +0330

    Refactor code

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import "fmt"
 
-func Print[T any](value T)T{
+func Print[T any](value T) T {
 	fmt.Println(value)
 	return value
 }
 
 func main() {
-	//peintstring("hello world")
-	//printinteger(12)
+	Print(12)
+	Print("hi my name is alireza")
+	panic(12.5)
+
 }
-//func peintstring(s string) string {
-	fmt.Println(s)
-	return s
-
-//}
-
-//func printinteger(i int) int {
-	//fmt.Println(i + 100)
-	//return i
-//}
